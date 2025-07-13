5f96f40ace42429e655f10cafa8d207104b2aa89
Mango-247 <zizzybuluba@gmail.com>
2025-07-12 19:31:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -190,7 +190,7 @@ window.addEventListener('DOMContentLoaded', () => {
     <svg width="100" height="24" xmlns="http://www.w3.org/2000/svg" style="overflow: visible;">
       <line x1="12" y1="12" x2="88" y2="12" stroke="#1c274c" stroke-width="4" stroke-linecap="round"/>
       <g id="thumb" cursor="pointer" transform="translate(12, 12)">
-        <svg fill="#425691" viewBox="-2 -2 24 24" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin" class="jam jam-volume-circle" width="20" height="20">
+        <svg fill="#5a73ad" viewBox="-2 -2 24 24" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin" class="jam jam-volume-circle" width="20" height="20">
           <path d="M10 20C4.477 20 0 15.523 0 10S4.477 0 10 0s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-2.664-4.759a2 2 0 0 1-2-2v-2.5a2 2 0 0 1 2-2H8.34l1.823-1.709a2 2 0 0 1 1.368-.54h.205a1.6 1.6 0 0 1 1.6 1.6v7.8a1.6 1.6 0 0 1-1.6 1.6h-.205a2 2 0 0 1-1.368-.542L8.34 13.241H7.336zm1.795-4.5H7.336v2.5H9.13l2.205 2.067V6.674L9.13 8.741z"/>
         </svg>
       </g>
