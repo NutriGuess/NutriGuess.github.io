54518a8264581b2cbcc3fe05ebf2132e707d2758
Mango-247 <zizzybuluba@gmail.com>
2025-07-12 20:07:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -219,7 +219,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   function setThumbPosition(volume) {
     // volume: 0 to 1
-    const x = trackStart + volume * trackLength - 20;
+    const x = trackStart + volume * trackLength - 13;
     thumb.setAttribute('transform', `translate(${x}, -1)`);
   }
 
